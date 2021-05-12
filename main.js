945c54d2bea09eb95cef095ef4f2a61eade3511a
Olivera Micic <omicic@gmail.com>
2021-05-12 12:30:30 +0200

    nav, typed.js, search button, language bar

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.element', {
+    // Waits 1000ms after typing "First"
+    strings: ['Arabic', 'Chinese','Croatian', 'Dutch','Finnish','French','English','Italia','Japanese'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+  });
